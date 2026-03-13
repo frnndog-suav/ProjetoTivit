728cc5861738241d199bf63aecd3e0fdf9e96ca5
Fernando <frnnd.goia97@gmail.com>
2026-03-13 14:59:08 -0300

    refactor: adding path alias

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,13 @@ import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
+    tsconfigPaths(),
   ],
 });
