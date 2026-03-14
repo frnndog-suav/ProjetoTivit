8337a16248c039b7364e0d0b914b8ec83a1745c3
Fernando <frnnd.goia97@gmail.com>
2026-03-14 13:44:34 -0300

    refactor: adjusting request interceptor and response interceptor

--- src/server/axios.ts
+++ src/server/axios.ts
@@ -1,5 +1,7 @@
+import { APP_PATHS } from "@routes/app-paths";
 import axios from "axios";
 import { SESSION_TOKEN } from "src/global/constants";
+import { decodeUserToken } from "src/global/decode-user-token";
 
 export const api = axios.create({
   baseURL: "/api/",
@@ -8,7 +10,6 @@ export const api = axios.create({
   },
 });
 
-// Interceptor para adicionar o token de autenticação
 api.interceptors.request.use((config) => {
   const accessToken = localStorage.getItem(SESSION_TOKEN);
 
@@ -18,3 +19,26 @@ api.interceptors.request.use((config) => {
 
   return config;
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      const accessToken = localStorage.getItem(SESSION_TOKEN);
+
+      if (accessToken) {
+        const decodedToken = decodeUserToken(accessToken);
+
+        if (decodedToken) {
+          const currentTime = Date.now() / 1000;
+
+          if (decodedToken.exp < currentTime) {
+            window.location.href = APP_PATHS.Login;
+          }
+        }
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
